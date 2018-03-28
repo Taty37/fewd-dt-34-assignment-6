ac19d90f4d80ad6c5b5cfaa717ef6368379d1f75
taty37 <tpiercy37@gmail.com>
2018-03-27 21:55:39 -0700

    append
    applied selectors

--- starter_code/js/script.js
+++ starter_code/js/script.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
 // loop through cities
 	cities.forEach(function(city){
 	var option = `<option>${city}</option>`
-	$("select").append(option);
+	$('select').append(option);
 	// create element
 	// append it to our select element
 
@@ -16,24 +16,25 @@ $(document).ready(function(){
 		//prevent page from performing the default action
 		event.preventDefault();
 //create a variable called "city" in order to store the city value that is submitted
-	$("select").change(addCity);
-
-		var city = $('#city-type').val();
+	
+	var city = $('#city-type').val();
 		console.log(city);
+	$('select').change(addCity);
+
 //use the city value to find out if when someone subnmits different values, it changes to the background image based on the city
 		if (city === 'New York' || city === 'NYC' || city === 'New York City' || city === 'new york' || city === 'nyc' || city === 'new york city') {
-			$('body').removeClass('starter-image').addClass('nyc');
+			$('body').attr("class", 'nyc');
 		} else if (city === 'San Francisco' || city === 'SF' || city === 'Bay Area' || city === 'san francisco' || city === 'sf' || city === 'bay area') {
-			$('body').removeClass('starter-image').addClass('sf');
+			$('body').attr("class", 'sf');
 		} else if (city === 'Los Angeles' || city === 'LA' || city === 'LAX' || city === 'los angeles' || city === 'la' || city === 'lax') {
-			$('body').removeClass('starter-image').addClass('la');
+			$('body').attr("class", 'la');
 		} else if (city === 'Austin' || city === 'ATX' || city === 'austin' || city === 'atx') {
-			$('body').removeClass('starter-image').addClass('austin');
+			$('body').attr("class", 'austin');
 		} else if (city === 'Sydney' || city === 'SYD' || city === 'sydney' || city === 'syd') {
-			$('body').removeClass('starter-image').addClass('sydney');
+			$('body').attr("class", 'sydney');
 		} else {
-			$('body').addClass('starter-image');	
+			$('body').attr("class", 'starter-image' )
 		}
 		}
-	})
+	})	
 });
