03e263a94b889cbc9073598824e22d70ff00b72b
taty37 <tpiercy37@gmail.com>
2018-03-28 19:16:17 -0700

    final commit
    completed

--- starter_code/js/script.js
+++ starter_code/js/script.js
@@ -11,30 +11,29 @@ $(document).ready(function(){
 	$('select').append(option);
 	// create element
 	// append it to our select element
-
-	function addCity(event) {
-		//prevent page from performing the default action
+})
+});
+		
+//create a variable called "city" in order to store the city value that is submitted	
+	$('select').change(function(event){
+// add .change function
+//prevent page from performing the default action
 		event.preventDefault();
-//create a variable called "city" in order to store the city value that is submitted
-	
+
 	var city = $('#city-type').val();
 		console.log(city);
-	$('select').change(addCity);
-
 //use the city value to find out if when someone subnmits different values, it changes to the background image based on the city
-		if (city === 'New York' || city === 'NYC' || city === 'New York City' || city === 'new york' || city === 'nyc' || city === 'new york city') {
+		if (city === 'New York') {
 			$('body').attr("class", 'nyc');
-		} else if (city === 'San Francisco' || city === 'SF' || city === 'Bay Area' || city === 'san francisco' || city === 'sf' || city === 'bay area') {
+		} else if (city === 'San Francisco') {
 			$('body').attr("class", 'sf');
-		} else if (city === 'Los Angeles' || city === 'LA' || city === 'LAX' || city === 'los angeles' || city === 'la' || city === 'lax') {
+		} else if (city === 'Los Angeles') {
 			$('body').attr("class", 'la');
-		} else if (city === 'Austin' || city === 'ATX' || city === 'austin' || city === 'atx') {
+		} else if (city === 'Austin') {
 			$('body').attr("class", 'austin');
-		} else if (city === 'Sydney' || city === 'SYD' || city === 'sydney' || city === 'syd') {
+		} else if (city === 'Sydney') {
 			$('body').attr("class", 'sydney');
 		} else {
 			$('body').attr("class", 'starter-image' )
 		}
-		}
-	})	
 });
